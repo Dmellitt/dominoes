bd3af44563be542eaa24ca45b24ab43367bd5ec0
David Mellitt <dmellitt@nd.edu>
2017-12-10 13:58:09 -0500

    physics/dragging compatibility

--- three/dominoes.js
+++ three/dominoes.js
@@ -1,5 +1,5 @@
 var container, stats;
-var camera, controls, scene, renderer;
+var camera, controls, scene, renderer, vec;
 var objects = [];
 
 Physijs.scripts.worker = 'three/physijs_worker.js';
@@ -10,28 +10,28 @@ animate();
 function init() {
     container = document.createElement( 'div' );
     document.body.appendChild( container );
-    camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.set( 1000, 50, 1500 );
+    camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 200 );
+    camera.position.set( 0, 3, 50 );
 
     scene = new Physijs.Scene();
     scene.background = new THREE.Color( 0x4085E8 );
     scene.add( new THREE.AmbientLight( 0x555555 ) );
     light = new THREE.DirectionalLight( 0xdfebff, 1 );
-    light.position.set( 50, 200, 100 );
+    light.position.set( 1, 4, 2 );
     light.position.multiplyScalar( 1.3 );
     light.castShadow = true;
     light.shadow.mapSize.width = 1024;
     light.shadow.mapSize.height = 1024;
-    var d = 2000;
+    var d = 40;
     light.shadow.camera.left = - d;
     light.shadow.camera.right = d;
     light.shadow.camera.top = d;
     light.shadow.camera.bottom = - d;
-    light.shadow.camera.far = 2000;
-    light.shadow.camera.near = -2000;
+    light.shadow.camera.far = 40;
+    light.shadow.camera.near = -40;
     scene.add( light );
 
-    var geometry = new THREE.BoxGeometry( 40, 40, 40 );
+    var geometry = new THREE.BoxGeometry( 2, 3, 0.3 );
 
     // ground
     var loader = new THREE.TextureLoader();
@@ -40,24 +40,18 @@ function init() {
     groundTexture.repeat.set( 5, 5 );
     groundTexture.anisotropy = 16;
     var groundMaterial = new THREE.MeshLambertMaterial( { map: groundTexture } );
-    var mesh = new Physijs.BoxMesh( new THREE.PlaneBufferGeometry( 3000, 3000 ), groundMaterial );
-    mesh.position.y =  - 250;
+    var mesh = new Physijs.BoxMesh( new THREE.PlaneBufferGeometry( 60, 60 ), groundMaterial );
+    mesh.position.y =  - 5;
     mesh.rotation.x = - Math.PI / 2;
     mesh.receiveShadow = true;
     scene.add( mesh );
 
-    // cubes
-    for ( var i = 0; i < 200; i ++ ) {
+    // dominoes
+    for ( var i = 0; i < 20; i ++ ) {
         var object = new Physijs.BoxMesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
-        object.position.x = Math.random() * 1000 - 500;
-        object.position.y = Math.random() * 600 - 100;
-        object.position.z = Math.random() * 800 - 400;
-        object.rotation.x = Math.random() * 2 * Math.PI;
-        object.rotation.y = Math.random() * 2 * Math.PI;
-        object.rotation.z = Math.random() * 2 * Math.PI;
-        object.scale.x = Math.random() * 2 + 1;
-        object.scale.y = Math.random() * 2 + 1;
-        object.scale.z = Math.random() * 2 + 1;
+        object.position.x = Math.random() * 20 - 10;
+        object.position.y = Math.random() * 12 - 2;
+        object.position.z = Math.random() * 16 - 8;
         object.castShadow = true;
         object.receiveShadow = true;
         scene.add( object );
@@ -74,11 +68,31 @@ function init() {
     // controls
     controls = new THREE.OrbitControls( camera, renderer.domElement );
     controls.maxPolarAngle = Math.PI * 0.5;
-    controls.minDistance = 1000;
-    controls.maxDistance = 5000;
+    controls.minDistance = 20;
+    controls.maxDistance = 100;
     var dragControls = new THREE.DragControls( objects, camera, renderer.domElement );
-    dragControls.addEventListener( 'dragstart', function ( event ) { controls.enabled = false; } );
-    dragControls.addEventListener( 'dragend', function ( event ) { controls.enabled = true; } );
+    vec = new THREE.Vector3;
+    dragControls.addEventListener( 'dragstart', function ( event ) {
+        controls.enabled = false;
+                                  
+        // freeze object
+        vec.set( 0, 0, 0 );
+        event.object.setLinearVelocity( vec );
+        event.object.setLinearFactor(vec);
+        event.object.setAngularVelocity( vec );
+        event.object.setAngularFactor(vec);
+    } );
+    dragControls.addEventListener( 'drag', function ( event ) {
+        event.object.__dirtyPosition = true;
+    } );
+    dragControls.addEventListener( 'dragend', function ( event ) {
+        controls.enabled = true;
+                                  
+        // unfreeze object
+        vec.set( 1, 1, 1 );
+        event.object.setAngularFactor(vec);
+        event.object.setLinearFactor(vec);
+    } );
     var info = document.createElement( 'div' );
     info.style.position = 'absolute';
     info.style.top = '10px';
